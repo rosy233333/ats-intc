4fb5a81137f51b9101304438e1550c1d7a1ef0af
Rosy <lc_rosy@qq.com>
2024-03-25 16:00:10 +0800

    增加存取usize类型的feature，"task-as-usize"

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,15 @@
 #![deny(missing_docs)]
 extern crate alloc;
 
+#[cfg(not(feature = "task-as-usize"))]
 mod task;
+#[cfg(not(feature = "task-as-usize"))]
 mod waker;
 
 use ats_intc_pac::*;
+#[cfg(not(feature = "task-as-usize"))]
 pub use task::*;
+#[cfg(not(feature = "task-as-usize"))]
 use waker::*;
 
 /// The Async Task Scheduler and Interrupt controller
@@ -30,6 +34,7 @@ impl AtsIntc {
     }
 
     /// Push a task into the prority scheduler
+    #[cfg(not(feature = "task-as-usize"))]
     pub fn ps_push(&self, task: TaskRef, priority: usize) {
         self.regs()
             .ats()
@@ -37,7 +42,17 @@ impl AtsIntc {
             .write(|w| unsafe { w.bits(task.as_ptr() as _) })
     }
 
+    /// Push a task into the prority scheduler
+    #[cfg(feature = "task-as-usize")]
+    pub fn ps_push(&self, task: usize, priority: usize) {
+        self.regs()
+            .ats()
+            .enqueue(priority)
+            .write(|w| unsafe { w.bits(task as _) })
+    }
+
     /// Fetch the task who has the highest priority from the hardware.
+    #[cfg(not(feature = "task-as-usize"))]
     pub fn ps_fetch(&self) -> Option<TaskRef> {
         let raw_task = self.regs().ats().dequeue().read().bits();
         if raw_task != 0 {
@@ -47,11 +62,32 @@ impl AtsIntc {
         }
     }
 
+    /// Fetch the task who has the highest priority from the hardware.
+    #[cfg(feature = "task-as-usize")]
+    pub fn ps_fetch(&self) -> Option<usize> {
+        let raw_task = self.regs().ats().dequeue().read().bits();
+        if raw_task != 0 {
+            Some(raw_task as usize)
+        } else {
+            None
+        }
+    }
+
     /// Push a interrupt handler into the interrupt controller
+    #[cfg(not(feature = "task-as-usize"))]
     pub fn intr_push(&self, irq: usize, handler: TaskRef) {
         self.regs()
             .intc()
             .intrenqueue(irq)
             .write(|w| unsafe { w.bits(handler.as_ptr() as _) })
+    }
+
+    /// Push a interrupt handler into the interrupt controller
+    #[cfg(feature = "task-as-usize")]
+    pub fn intr_push(&self, irq: usize, handler: usize) {
+        self.regs()
+            .intc()
+            .intrenqueue(irq)
+            .write(|w| unsafe { w.bits(handler as _) })
     }
 }
